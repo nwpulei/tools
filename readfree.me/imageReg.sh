dce0a28d26186945f491f31ffdfba8302ec99a89
lei.lei <lei.lei@qunar.com>
2019-02-15 11:26:01 +0800

    修复检查md5是否安装bug,修复md5sum 值错误bug, 修复date 缺少参数bug

--- readfree.me/imageReg.sh
+++ readfree.me/imageReg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 #采用 http://www.fateadm.com/ 识别验证码
 
 #$1 图片路径 $2 PD账号 $3 PD秘钥
@@ -11,18 +11,13 @@ ukey="$3"
 #图片类型 http://www.fateadm.com/price.html
 predict_type=20400
 
-
-
-now=`date`
-timestamp=`date -j -f "%a %b %d %T %Z %Y" "$now" "+%s"`
-
-
 function calcMd5(){
-	if [ -n `which md5` ];then
+	which md5 >/dev/null
+	if [ 0 -eq $? ];then
 		#mac have md5,but md5sum cann't found
 		md5 -q -s "$1"
 	else
-		echo "$1" |  md5sum - | awk '{print $1}'
+		echo -n "$1" |  md5sum - | awk '{print $1}'
 	fi
 }
 
@@ -32,15 +27,19 @@ function calcSign(){
 	calcMd5 "$1$3$csign"
 }
 
-sign=`calcSign "$user_id" "$ukey" "$timestamp"`
 img_data=`base64 -i "$imagePath" | sed 's/+/%2B/g;s/\//%2F/g;s/=/%3D/g'`
+
+#set -x
+
+timestamp=`date +%s`
+sign=`calcSign "$user_id" "$ukey" "$timestamp"`
 data="user_id=$user_id&timestamp=$timestamp&sign=$sign&predict_type=$predict_type&img_data=$img_data"
 
 respon=`curl 'http://pred.fateadm.com/api/capreg' -H 'Pragma: no-cache' -H 'Origin: http://www.fateadm.com' \
 -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' \
 -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36' \
 -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Cache-Control: no-cache' \
---data "$data" --compressed -vv`
+--data "$data" --compressed -vv 2>/dev/null`
 echo $respon >&2
 #{"RetCode":"0","ErrMsg":"","RequestId":"20xxxxxxxxxx","RspData":"{\"result\": \"uaak\"}"}
 echo "$respon" | sed 's/\\//g'| grep -o  "result.*" | awk -F'"' '{print $3}'
